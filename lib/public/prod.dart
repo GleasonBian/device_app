5e8705f5854d999838b7dc58ad23f4c33400f473
Gleason <bianliuzhu@gmail.com>
2020-07-04 12:00:55 +0800

    Merge branch 'master' of F:\flutter_template with conflicts.

--- lib/public/prod.dart
+++ lib/public/prod.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_template/public/config.dart';
 void main() {
   var configuredApp = new ENV(
-    appName: '设备管理系统',
+    appName: 'app 名称',
     envName: 'prod',
     baseUrl: 'http://crcc.sjgtw.com:8090/',
     child: new MyApp(),
